ae5bac0d19287ad94c93bf21bc96b8cfa4390196
Renan Holler <rrholler@gmail.com>
2023-11-30 22:07:21 -0300

    fix gitignore

--- src/main/java/com/controleestoque/exceptions/EnderecoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.controleestoque.exceptions;
-
-public class EnderecoNotFoundException extends RuntimeException{
-    public EnderecoNotFoundException(){
-        super("Endereço não encontrado.");
-    }
-}
