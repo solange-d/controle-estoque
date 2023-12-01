86c9ff3db473ed1c672b9de1affeed79650a2f6e
Renan Holler <rrholler@gmail.com>
2023-11-30 22:26:47 -0300

    fix gitignore

--- src/main/java/com/controleestoque/exceptions/FornecedorNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.controleestoque.exceptions;
-
-public class FornecedorNotFoundException extends RuntimeException{
-    public FornecedorNotFoundException(){
-        super("Fornecedor não encontrado.");
-    }
-}
--- src/main/java/com/controleestoque/exceptions/ProdutoNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.controleestoque.exceptions;
-
-public class ProdutoNotFoundException extends RuntimeException{
-
-    public ProdutoNotFoundException(){
-        super("Produto não encontrado.");
-    }
-}
