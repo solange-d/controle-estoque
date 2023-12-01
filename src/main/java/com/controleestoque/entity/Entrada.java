50337e30c9e80cfc4defc978d3061cd6da4a688f
Renan Holler <rrholler@gmail.com>
2023-11-30 22:00:52 -0300

    Removidos imports

--- src/main/java/com/controleestoque/entity/Entrada.java
+++ src/main/java/com/controleestoque/entity/Entrada.java
@@ -8,7 +8,6 @@ import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 @Data
--- src/main/java/com/controleestoque/entity/Movimento.java
+++ src/main/java/com/controleestoque/entity/Movimento.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
